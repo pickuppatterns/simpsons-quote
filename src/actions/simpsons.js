8d8c377a291250498796d603830900a55132028d
pickuppatterns <pickup.patterns@gmail.com>
2019-03-10 16:59:18 -0700

    updates actions for character & img

--- src/actions/simpsons.js
+++ src/actions/simpsons.js
@@ -1,9 +1,23 @@
-import { getQuotes } from '../services/simpsonsApi';
+import { getQuotes, getCharacterName, getCharacterImg } from '../services/simpsonsApi';
 
 
 export const FETCH_QUOTES = 'FETCH_QUOTES';
+export const FETCH_CHARACTER = 'FETCH_CHARACTER';
+export const FETCH_IMG = 'FETCH_IMG';
 
 export const fetchQuotesWithPromise = () => ({
   type: FETCH_QUOTES,
   payload: getQuotes()
+});
+
+
+export const fetchCharacterWithPromise = () => ({
+  type: FETCH_CHARACTER,
+  payload: getCharacterName()
+});
+
+
+export const fetchImgWithPromise = () => ({
+  type: FETCH_IMG,
+  payload: getCharacterImg()
 });
