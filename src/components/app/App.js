59932adf46aedb0795526904a59ea20e6a1b2765
pickuppatterns <pickup.patterns@gmail.com>
2019-03-10 17:24:12 -0700

    fixes App component logic BIG TIME!

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import store from '../../store';
 import AllQuotes from '../../containers/AllQuotes';
-import { fetchQuotesWithPromise } from '../../actions/simpsons';
+
 
 export default function App() {
-  store.dispatch(fetchQuotesWithPromise(10));
-  return <>
+  return (
+<>
   <h1>SIMPSONS QUOTES</h1>
   <AllQuotes />
-  </>;
+  </>
+  );
 }
 
