9455577a12d64a3b70f203adf3a1bda414ffa244
pickuppatterns <pickup.patterns@gmail.com>
2019-03-11 08:04:13 -0700

    quotes, character render in browser image does not

--- src/containers/AllQuotes.js
+++ src/containers/AllQuotes.js
@@ -1,11 +1,34 @@
 import { connect } from 'react-redux';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { getQuotes, getCharacterName, getCharacterImg } from '../selectors/simpsons';
 import { fetchQuotesWithPromise } from '../actions/simpsons';
 import Quotes from '../components/quotes/Quotes';
   
 
 
+class AllQuotes extends PureComponent {
+  static propTypes = {
+    quote: PropTypes.string.isRequired,
+    characterName: PropTypes.string.isRequired,
+    characterImg: PropTypes.string.isRequired,
+    fetch: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    return (
+      <>
+        <Quotes {...this.props} />
+      </>
+    );
+  }
+}
 const mapStateToProps = state => ({
+
   quote: getQuotes(state),
   characterName: getCharacterName(state),
   characterImg: getCharacterImg
@@ -22,5 +45,5 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Quotes);
+)(AllQuotes);
 
