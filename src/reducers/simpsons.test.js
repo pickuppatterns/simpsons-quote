236c3767f75921db07aa3bfc8b28f2af9974ad3e
pickuppatterns <pickup.patterns@gmail.com>
2019-03-14 12:52:58 -0700

    updates failing test

--- src/reducers/simpsons.test.js
+++ src/reducers/simpsons.test.js
@@ -3,24 +3,24 @@ import { FETCH_QUOTES } from '../actions/simpsons';
 
 describe('simpsons reducer', () => {
   it('handles a fetch quotes action', () => {
-    const state = {
+    const state = [{
       quote: '',
       characterName:'',
       characterImg:''
-    };
-    const fetchQuotesWithAPromise = reducer(state, {
+    }];
+    const fetchQuotesWithAPromise = reducer(state, [{
       type: FETCH_QUOTES,
       payload: {
         quote: '',
         characterName:'',
         characterImg:''
       }
-    });
-    expect(fetchQuotesWithAPromise).toEqual({
+    }]);
+    expect(fetchQuotesWithAPromise).toEqual([{
       quote: '',
       characterName:'',
       characterImg:''
-    });
+    }]);
   });
 
 });
