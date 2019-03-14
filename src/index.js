9cbaa3c22b90f502be04b3a032f28461ebb21497
pickuppatterns <pickup.patterns@gmail.com>
2019-03-14 13:14:08 -0700

    updates eslint error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './components/app/App'
+import App from './components/app/App';
 
 render(
   <Provider store={store}>
@@ -10,3 +10,4 @@ render(
   </Provider>,
   document.getElementById('root')
 );
+
